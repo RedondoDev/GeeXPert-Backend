a0d8acbe2120b72b3486b3d9e047de640df979bc
RedondoDEV <javierRedondoDEV@gmail.com>
2025-04-22 21:19:38 +0200

    Added exception handler

---
+++ src/main/java/com/redondodev/GeeXPert_back/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.redondodev.GeeXPert_back.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_GATEWAY);
+    }
+
+}
